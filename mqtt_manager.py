8894a1a94df60c9b235af9e1058a6fdc77743918
Gabriel Weber <wgabrielweber@gmail.com>
2024-12-28 23:46:00 -0300

    corrected parameter passing between files

--- mqtt_manager.py
+++ mqtt_manager.py
@@ -7,6 +7,7 @@ class MQTTManager:
         self.broker_address = broker_address
         self.command_topic = command_topic
         self.data_topic = data_topic
+        self.sensor_param = None
 
         # Set callbacks
         self.client.on_message = self.on_message
@@ -25,25 +26,31 @@ class MQTTManager:
 
     def publish(self, topic, message):
         """Publish a message to a topic."""
+        print(f'publishing "{message}" on {topic}')
         self.client.publish(topic, message)
 
     def subscribe_to_data_topic(self):
         """Subscribe to the data topic."""
+        print(f'Subscribed to {self.data_topic}')
         self.client.subscribe(self.data_topic)
+
+    def update_sensor_param(self, param):
+        """Update the sensor parameter."""
+        self.sensor_param = param
 
     def on_message(self, client, userdata, msg):
         """Handle incoming MQTT messages."""
         if msg.topic == self.data_topic:
-            # Use session state to get the selected sensor parameter from the Streamlit app
-            from app import st
-            selected_sensor_param = st.session_state.selected_sensor_param
-            self.handle_data_message(msg.payload.decode(), selected_sensor_param)
+            print(f"Message received at {self.data_topic}. Sensor Parameter: {self.sensor_param}")
+
+            if self.sensor_param:
+                self.handle_data_message(msg.payload.decode(), self.sensor_param)
+            else:
+                print("Sensor parameter not initialized!")
 
     def handle_data_message(self, message, sensor_param):
         """Pass the message and sensor_param to the parse_message function."""
         try:
-            # Call parse_message function
             data_parser.parse_message(message, sensor_param)
-            print(f"Message processed. Sensor parameters: {sensor_param}")
         except Exception as e:
             print(f"Failed to process message: {e}")
